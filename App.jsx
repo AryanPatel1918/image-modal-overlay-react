3c2fa4efac5ee4a3044fb691a184ad45fc43783c
Aryan Patel <97249070+AryanPatel1918@users.noreply.github.com>
2025-11-29 16:16:42 -0500

    Implement loading state and fetch APOD data
    Added loading state and APOD data fetching logic.

--- App.jsx
+++ App.jsx
@@ -1,36 +1,58 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 export default function App() {
+    const [loading, setLoading] = useState(false)
+    const [apodData, setApodData] = useState(null)
     const [modalOpen, setModalOpen] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        const url = "https://api.nasa.gov/planetary/apod?api_key="
+        const NASA_API_KEY = "Nc4u3rKeDSGDu6oTRcpkb9JwjUAGLGddaSc3LdXs"
+        fetch(url + NASA_API_KEY)
+            .then(res => res.json())
+            .then(data => {
+                setApodData(data)
+                setLoading(false)
+            })
+    }, [])
     
     return (
         <div style={{ width: "400px", height: "250px", position: "relative" }}>
-            <img style={{ width: "100%", height: "100%", objectFit: "cover" }} src="https://images.unsplash.com/photo-1526779259212-939e64788e3c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D" />
-            <div style={{ position: "absolute", bottom: 0, color: "white", padding: "10px", display: "flex", flexDirection: "column", gap: "5px" }}>
-                <h1 style={{ margin: 0, fontSize: "19px" }}>The River of Mount Olympus</h1>
-                <p style={{ margin: 0, fontSize: "13px" }}>NASA | Astronomy Picture of the Day | 2025-11-25</p>
-            </div>
-            {modalOpen ? (
-                <div style={{ position: "absolute", top: 0, right: 0, width: "35%", height: "100%", backgroundColor: "rgba(0, 0, 0, 0.7)", color: "white", display: "flex" }}>
-                    <div style={{ padding: "12px", paddingBottom: "30px", paddingRight: "12px", maxHeight: "100%", overflowY: "auto", scrollbarWidth: "thin", display: "flex", flexDirection: "column", gap: "5px" }}>
-                        <h3 style={{ fontSize: "15px", fontWeight: "500", margin: 0, overflowWrap: "break-word" }}>The River of Mount Olympus</h3>
-                        <p style={{ fontSize: "12px", fontWeight: "500", margin: 0, overflowWrap: "break-word" }}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae luctus dui. Vivamus viverra, enim at malesuada elella. Duis a gravida augue. Nunc placerat eonec ut eleifend turpis.
-                        </p>
+            {apodData ? ( 
+                <>
+                    <img style={{ width: "100%", height: "100%", objectFit: "cover" }} src="https://images.unsplash.com/photo-1526779259212-939e64788e3c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D" />
+                    <div style={{ position: "absolute", bottom: 8, left: 10, color: "white", display: "flex", flexDirection: "column", gap: "2px" }}>
+                        <p style={{ margin: 0, fontSize: "13px", overflowWrap: "break-word" }}>NASA | Astronomy Picture of the Day | {apodData.date}</p>
+                        <h1 style={{ margin: 0, fontSize: "19px", overflowWrap: "break-word" }}>{apodData.title}</h1>
                     </div>
-                    <button onClick={() => setModalOpen(false)} style={{ position: "absolute", bottom: 5, left: 5, fontSize: "20px", cursor: "pointer", color: "white", backgroundColor: "transparent", border: "none" }}>
-                        →
-                    </button>
-                </div>
+                    {modalOpen ? (
+                        <div style={{ position: "absolute", top: 0, right: 0, width: "35%", height: "100%", backgroundColor: "rgba(0, 0, 0, 0.7)", color: "white", display: "flex" }}>
+                            <div style={{ padding: "12px", paddingBottom: "30px", paddingRight: "12px", maxHeight: "100%", overflowY: "auto", scrollbarWidth: "thin", display: "flex", flexDirection: "column", gap: "5px" }}>
+                                <h3 style={{ fontSize: "15px", margin: 0, overflowWrap: "break-word" }}>{apodData.title}</h3>
+                                <p style={{ fontSize: "12px", margin: 0, overflowWrap: "break-word" }}>
+                                    {apodData.explanation}
+                                </p>
+                            </div>
+                            <button onClick={() => setModalOpen(false)} style={{ position: "absolute", bottom: 5, left: 5, fontSize: "20px", cursor: "pointer", color: "white", backgroundColor: "transparent", border: "none" }}>
+                                →
+                            </button>
+                        </div>
+                    ) : (
+                        <button
+                            onClick={() => setModalOpen(true)}
+                            onMouseEnter={e => e.currentTarget.style.opacity = 0.8}
+                            onMouseLeave={e => e.currentTarget.style.opacity = 1}
+                            title="Info"
+                            style={{ position: "absolute", bottom: 10, right: 10, border: "none", width: "25px", height: "25px", cursor: "pointer", padding: "2px" , backgroundColor: "white", borderRadius: "50%", display: "flex", justifyContent: "center", alignItems: "center", fontSize: "15px", fontWeight: "bold" }}
+                        >
+                            i
+                        </button>
+                    )}
+                </>
             ) : (
-                <button
-                    onClick={() => setModalOpen(true)}
-                    onMouseEnter={e => e.currentTarget.style.opacity = 0.8}
-                    onMouseLeave={e => e.currenTarget.style.opacity = 1}
-                    title="Info"
-                    style={{ position: "absolute", bottom: 10, right: 10, border: "none", width: "25px", height: "25px", cursor: "pointer", padding: "2px" , backgroundColor: "white", borderRadius: "50%", display: "flex", justifyContent: "center", alignItems: "center", fontSize: "15px", fontWeight: "bold" }}
-                >
-                    i
-                </button>
+                <div style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0, backgroundColor: "black",  display: "flex", justifyContent: "center", alignItems: "center" }}>
+                    <h3 style={{ color: "white", margin: 0, fontWeight: "500" }}>Loading...</h3>
+                </div>
             )}
         </div>
     )
